75193e50a226cfd67212e137f94ba5292a6a9a8d
Rowan Victor <209355544+rowanvictor01@users.noreply.github.com>
2025-12-29 16:16:51 +0800

    Add onedark theme, change lualine theme to powerline_dark, and refactor code

--- init.lua
+++ init.lua
@@ -1,137 +1,122 @@
+
 -----------------------
 -- BASIC SETUP
 -----------------------
 
-vim.cmd("syntax on")
-vim.o.termguicolors = true
---vim.cmd("colorscheme habamax")
-vim.g.have_nerd_font = true
-vim.o.mouse = "a"
-vim.schedule(function()
-	vim.o.clipboard = "unnamedplus"
-end)
-vim.o.breakindent = true
-vim.o.undofile = true
-vim.o.number = true
-vim.o.relativenumber = true
-vim.o.ignorecase = true
-vim.o.smartcase = true
-vim.o.inccommand = "split"
-vim.o.cursorline = true
-vim.o.scrolloff = 16
-vim.o.confirm = true
-vim.keymap.set("n", "<Esc>", "<cmd>noh<CR>")
-
-
-
-----------------------
--- PLUGIN MANAGER
-----------------------
-
--- Bootstrap lazy.nvim
-local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
-if not (vim.uv or vim.loop).fs_stat(lazypath) then
-  local lazyrepo = "https://github.com/folke/lazy.nvim.git"
-  local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
-  if vim.v.shell_error ~= 0 then
-    vim.api.nvim_echo({
-      { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
-      { out, "WarningMsg" },
-      { "\nPress any key to exit..." },
-    }, true, {})
-    vim.fn.getchar()
-    os.exit(1)
-  end
-end
-vim.opt.rtp:prepend(lazypath)
-
--- Make sure to setup `mapleader` and `maplocalleader` before
--- loading lazy.nvim so that mappings are correct.
--- This is also a good place to setup other settings (vim.opt)
 vim.g.mapleader = " "
 vim.g.maplocalleader = "\\"
 
--- [Setup lazy.nvim]
+vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
+vim.keymap.set("n", "<Esc>", "<cmd>noh<CR>")
+
+vim.opt.termguicolors = true
+vim.opt.mouse = "a"
+
+-- Clipboard (delay to avoid startup issues)
+vim.schedule(function()
+  vim.opt.clipboard = "unnamedplus"
+end)
+
+-- UI / editing
+vim.opt.number = true
+vim.opt.relativenumber = true
+vim.opt.cursorline = true
+vim.opt.scrolloff = 16
+vim.opt.breakindent = true
+vim.opt.undofile = true
+vim.opt.confirm = true
+
+-- Searching
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+vim.opt.inccommand = "split"
+
+vim.g.have_nerd_font = true
+
+
+-----------------------
+-- PLUGIN MANAGER
+-----------------------
+
+-- Bootstrap lazy.nvim
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.uv.fs_stat(lazypath) then
+  vim.fn.system({
+    "git", "clone", "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable",
+    lazypath,
+  })
+end
+vim.opt.rtp:prepend(lazypath)
+
+
+-----------------------
+-- PLUGINS
+-----------------------
+
 require("lazy").setup({
-  spec = {
-    -- add your plugins here
+
+
+  -- Colorscheme (load first)
+  {
+    "navarasu/onedark.nvim",
+    priority = 1000,
+    config = function()
+      require("onedark").setup {
+	      style = "darker"
+      }
+      require("onedark").load()
+    end
+  },
+
+
+  -- Statusline
+  {
     "nvim-lualine/lualine.nvim",
     dependencies = { "nvim-tree/nvim-web-devicons" },
-
-
-    'nvim-treesitter/nvim-treesitter',
-    lazy = false,
-    build = ':TSUpdate'
+    config = function()
+      require("lualine").setup({
+        options = {
+          icons_enabled = true,
+          theme = "powerline_dark",
+          component_separators = { left = "", right = "" },
+          section_separators = { left = "", right = "" },
+          globalstatus = false,
+        },
+      })
+    end,
   },
-  -- Configure any other settings here. See the documentation for more details.
-  -- colorscheme that will be used when installing plugins.
-  install = { colorscheme = { "default" } },
-  -- automatically check for plugin updates
+
+
+  -- Tree-sitter
+  {
+    "nvim-treesitter/nvim-treesitter",
+    build = ":TSUpdate",
+    config = function()
+      require("nvim-treesitter").setup({
+        auto_install = true,
+
+        ensure_installed = {
+          "bash", "c", "html", "lua", "luadoc",
+          "markdown", "markdown_inline",
+          "vim", "vimdoc", "python", "javascript",
+        },
+
+        highlight = {
+          enable = true,
+        },
+
+        indent = {
+          enable = true,
+        },
+      })
+    end,
+  },
+
+}, {
   checker = { enabled = true },
 })
 
+--vim.cmd.colorscheme("catppuccin-mocha")
 
--- [Setup lualine.nvim]
-require('lualine').setup {
-  options = {
-    icons_enabled = true,
-    theme = '16color',
-    component_separators = { left = '', right = ''},
-    section_separators = { left = '', right = ''},
-    disabled_filetypes = {
-      statusline = {},
-      winbar = {},
-    },
-    ignore_focus = {},
-    always_divide_middle = true,
-    always_show_tabline = true,
-    globalstatus = false,
-    refresh = {
-      statusline = 1000,
-      tabline = 1000,
-      winbar = 1000,
-      refresh_time = 16, -- ~60fps
-      events = {
-        'WinEnter',
-        'BufEnter',
-        'BufWritePost',
-        'SessionLoadPost',
-        'FileChangedShellPost',
-        'VimResized',
-        'Filetype',
-        'CursorMoved',
-        'CursorMovedI',
-        'ModeChanged',
-      },
-    }
-  },
-  sections = {
-    lualine_a = {'mode'},
-    lualine_b = {'branch', 'diff', 'diagnostics'},
-    lualine_c = {'filename'},
-    lualine_x = {'encoding', 'fileformat', 'filetype'},
-    lualine_y = {'progress'},
-    lualine_z = {'location'}
-  },
-  inactive_sections = {
-    lualine_a = {},
-    lualine_b = {},
-    lualine_c = {'filename'},
-    lualine_x = {'location'},
-    lualine_y = {},
-    lualine_z = {}
-  },
-  tabline = {},
-  winbar = {},
-  inactive_winbar = {},
-  extensions = {}
-}
-
-
--- [Setup Treesitter.nvim]
-require'nvim-treesitter'.install { 'c', 'python', 'lua'}
-
-vim.api.nvim_create_autocmd('FileType', {
-  pattern = { '<filetype>' },
-  callback = function() vim.treesitter.start() end,
-})
